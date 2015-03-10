0d125323bf00848ec4995e6d990cba5bfa846035
Jay Wengrow <jaywngrw@gmail.com>
2015-03-09 20:12:07 -0500

    implemented create action

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -10,4 +10,9 @@ class PeopleController < ApplicationController
     @person = Person.find(params[:id])
     respond_with(@person)
   end
+
+  def create
+    @person = Person.create(:prefix => params[:prefix], :first_name => params[:first_name], :middle_name => params[:middle_name], :last_name => params[:last_name], :suffix => params[:suffix], :email => params[:email], :phone => params[:phone])
+    respond_with(@person)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/people' => 'people#index'
-  get '/people/:id' => 'people#show'
+  get '/people/:id' => 'people#show', :as => :person
+  post '/people' => 'people#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
